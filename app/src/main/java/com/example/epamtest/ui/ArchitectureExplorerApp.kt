ab5d3caab6b3ff45e97335ba328e0213fc79fe65
Rubianoch <rubianoch@MacBook-Pro-de-Ana.local>
2026-08-12 18:51:01 -0500

    Init

---
+++ app/src/main/java/com/example/epamtest/ui/ArchitectureExplorerApp.kt
@@ -0,0 +1,20 @@
+package com.example.epamtest.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ArchitectureExplorerApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Text(
+            text = "Architecture Explorer",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
