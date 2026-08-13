709531a4acaab3cf2825e1568b541982b3519e9b
Rubianoch <rubianoch@MacBook-Pro-de-Ana.local>
2026-08-12 19:03:03 -0500

    Topics section added

--- app/src/main/java/com/example/epamtest/MainActivity.kt
+++ app/src/main/java/com/example/epamtest/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.epamtest.ui.ArchitectureExplorerApp
 import com.example.epamtest.ui.theme.EpamTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             EpamTestTheme {
-
+                ArchitectureExplorerApp()
             }
         }
     }
--- app/src/main/java/com/example/epamtest/ui/ArchitectureExplorerApp.kt
+++ app/src/main/java/com/example/epamtest/ui/ArchitectureExplorerApp.kt
@@ -3,18 +3,19 @@ package com.example.epamtest.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.epamtest.ui.topics.TopicsScreen
 
 @Composable
 fun ArchitectureExplorerApp() {
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
-        Text(
-            text = "Architecture Explorer",
+
+        TopicsScreen(
             modifier = Modifier.padding(innerPadding)
         )
+
     }
 }
